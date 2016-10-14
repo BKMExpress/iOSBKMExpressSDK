f15a94fc74fc14c35a8cdc79c84a1f6e8c4c77aa
Kadir Guzel <kguzel@bkm.com.tr>
2016-10-14 16:59:59 +0300

    Project files

---
+++ SDKTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDKTestApp
+//
+//  Created by Kadir Guzel on 10/14/16.
+//  Copyright © 2016 Kadir Guzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
